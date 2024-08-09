added jitize and fixed masking issue

hasasia_mod_KG/sensitivity.py
@@ -446,12 +446,17 @@ class RRF_Spectrum(object):
     def Cgw(self):
         """Gravitational Wave Red Noise Covariance Matrix
         """
-        nf_gw = self.freqs_gwb.size
         nf = self.freqs_rn.size
+        nf_gw = self.freqs_gwb.size
 
-        # Create a mask to create frequencies for gwb
-        mask = np.isin(self.freqs_rn, self.freqs_gwb)
-        mask= np.repeat(mask, 2)
+        mask = np.full(nf, False)
+        for i in range(nf):
+            for j in range(nf_gw):
+                if np.isclose(self.freqs_rn[i], self.freqs_gwb[j], rtol=1e-5, atol=0):
+                    mask[i] = True
+                    continue
+        #duplicates the mask for use of 2Nfreq formalism
+        mask_rp = np.repeat(mask, 2)
       
         gwb_power = red_noise_powerlaw(A=self.amp_gw, gamma=self.gamma_gw, freqs=self.freqs_gwb)
         C_gwbproto = np.zeros((2*nf_gw, 2*nf_gw))
@@ -460,9 +465,7 @@ class RRF_Spectrum(object):
         del gwb_power
 
         C_gwb = np.zeros((2*nf, 2*nf))
-
-        masked_indices = np.where(mask)[0]
-        C_gwb[np.ix_(masked_indices, masked_indices)] = C_gwbproto
+        C_gwb[np.ix_(mask_rp, mask_rp)] = C_gwbproto
         return C_gwb/get_Tspan([self])
     
 
@@ -499,30 +502,17 @@ class RRF_Spectrum(object):
             _type_: _description_
         """
         #Defining Ncal and NcalInv depending on existence of self.N or self.K_inv
-        T = self.toas.max()-self.toas.min()
-        phi = (self.Cgw + self.Cirn)
-        phi_inv = jnp.linalg.inv(phi)
-        del phi
-
-        Sigma = (phi_inv + jnp.matmul(self.Z, self.J)).T
-        SigmaInv = jnp.linalg.inv(Sigma)
-        del Sigma
-        
-        Gtilde = jnp.zeros((self.freqs.size,self.G.shape[1]),dtype='complex128')
-        Gtilde = jnp.dot(jnp.exp(1j*2*jnp.pi*self.freqs[:,jnp.newaxis]*self.toas),self.G)
-
-        NcalInv_ = self.K_inv - jnp.matmul(self.Z.T, jnp.matmul(SigmaInv, self.Z))#divide by 2 for some reason   
-
-        del SigmaInv
-        #divided by 4, not 2 for some reason, possibly some normalization stuff
-        TfN = jnp.matmul(jnp.conjugate(Gtilde),jnp.matmul(NcalInv_,Gtilde.T)) / 2
-
-        if return_Gtilde_Ncal:
-            return jnp.real(TfN), Gtilde, jnp.linalg.inv(NcalInv_)
-        elif full_matrix:
-            return jnp.real(TfN)
-        else:
-            return jnp.real(jnp.diag(TfN)) / T
+        if not hasattr(self, '_NcalInv'):
+            phi = jnp.array((self.Cgw + self.Cirn))
+            K_inv = jnp.array(self.K_inv)
+            G = jnp.array(self.G)
+            J = jnp.array(self.J)
+            Z = jnp.array(self.Z)
+            toas = jnp.array(self.toas)
+            freqs = jnp.array(self.freqs)
+            self._NcalInv = get_NcalInv_RRF(K_inv, G, phi, J,
+                    Z, freqs, toas, full_matrix=full_matrix, return_Gtilde_Ncal=return_Gtilde_Ncal)
+        return self._NcalInv
             
     @property
     def P_n(self):